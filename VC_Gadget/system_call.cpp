0ad6f9d1a7ca26ce4d34832dce9664555f2bd142
TaikorRoy <furaoing@qq.com>
2015-06-12 18:37:33 +0800

    v0.1.1
    Added Some Win32 ShellExecutation Functionality

--- VC_Gadget/system_call.cpp
+++ VC_Gadget/system_call.cpp
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <windows.h>
 #include <ostream>
+#include <string>
 
 using namespace std;
 
@@ -13,15 +14,55 @@ string ExePath() {
 	return string(buffer);
 }
 
+/*
+HINSTANCE launch_webbot(char* project_base_path)
+{
+	const char* command = "C:\\Anaconda2\\python.exe";
+	string webbot_path = project_base_path + "cosmetic_webbot\\main.py";
+	const char* file = webbot_path.c_str();
+	cout << command;
+	cout << "\n";
+	cout << file;
+	HINSTANCE a = ShellExecute(NULL, command, file, NULL, NULL, 5);
+	return a;
+}
+
+HINSTANCE launch_webserver(char* project_base_path)
+{
+	const char* command = "C:\\Anaconda2\\python.exe";
+	string webbot_path = project_base_path + "web_server\\ws_app.py";
+	const char* file = webbot_path.c_str();
+	cout << command;
+	cout << "\n";
+	cout << file;
+	HINSTANCE a = ShellExecute(NULL, command, file, NULL, NULL, 5);
+	return a;
+}
+*/
+
 int main()
 {
-	string s = ExePath();
-	string web_bot = s + "cosmetic_webbot\\main.py";
-	a = system(web_bot);
-	Sleep(3000);
-	
-	string s = ExePath();
-	//printf("%s\n", s.c_str());
-	Sleep(30000);
+	string manager_path = ExePath();
+	int manager_path_strlen = manager_path.length();
+	string project_base_path = manager_path.replace(manager_path_strlen - 13, manager_path_strlen, "");
 
+	const char* command = "C:\\Anaconda2\\python.exe";
+	string webbot_path = project_base_path + "cosmetic_webbot\\main.py";
+	const char* file = webbot_path.c_str();
+	cout << command;
+	cout << "\n";
+	cout << file;
+	HINSTANCE a = ShellExecute(NULL, command, file, NULL, NULL, 5);
+
+	const char* command1 = "C:\\Anaconda2\\python.exe";
+	string webbot_path1 = project_base_path + "web_server\\ws_app.py";
+	const char* file1 = webbot_path.c_str();
+	cout << command1;
+	cout << "\n";
+	cout << file1;
+	HINSTANCE b = ShellExecute(NULL, command1, file1, NULL, NULL, 5);
+
+	Sleep(500000);
+
+	
 }
